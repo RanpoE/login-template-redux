7ec70e395fec228e5eace4334993a53ff0ab39d2
Edward <edward.nayve@boom.ai>
2024-02-03 09:32:23 +0800

    update config

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -79,7 +79,7 @@ const Login = () => {
                       <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                     </div>
                   </div>
-                  <a href='#' className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                  <a href='/#' className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                 </div>
                 <Button type='submit' variant="primary w-full" text='Log in' />
                 {/* <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button> */}

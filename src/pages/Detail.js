a273431460516dac42e4ba8fd1946d6073ebb3e1
Edward <edward.nayve@boom.ai>
2025-04-12 12:14:27 +0800

    Update layout

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -7,24 +7,40 @@ function Detail() {
     const posts = useSelector(state => state.post)
     const [detail] = useState(posts?.data?.find(post => post._id === params.id))
 
-    const styleStr = "w-full max-w-7xl pt-20 pb-10 mb-10 columns-1 space-y-5 mx-auto"
+    const styleStr = "w-full max-w-7xl pt-20 pb-10 mb-10 space-y-5 mx-auto"
 
     if (!detail) return (<div className={styleStr}>Invalid ID provided.</div>)
     return (
-        <div className={styleStr}>
-            <div class="flex font-sans">
-                <div class="flex-none w-48 relative">
+        <div className="flex flex-col md:flex-row pt-20">
+            <div className='md:w-2/3'>
+                <div class="relative">
                     <a href={detail.photo}>
-                        <img src={detail.photo} alt={detail.caption} class="absolute inset-0 w-full h-[200px] object-cover" loading="lazy" />
+                        <img src={detail.photo} alt={detail.caption} class="inset-0 w-full h-[420px] object-cover" loading="lazy" />
                     </a>
                 </div>
-                <form class="flex-auto p-6">
-                    <div class="flex flex-wrap">
-                        <h1 class="flex-auto text-lg font-semibold text-slate-900">
+            </div>
+            <div className='md:w-1/3 p-2 text-center'>
+                    <h1 class="text-lg font-semibold text-slate-900">
+                        {detail.caption}
+                    </h1>
+                    <div class="text-sm font-medium text-slate-700 mt-2">
+                        Created by: <code>{detail.user}</code>
+                    </div>
+            </div>
+
+            {/* <div class="flex font-sans mt-10 max-sm:flex-col">
+                <div class="w-screen relative">
+                    <a href={detail.photo}>
+                        <img src={detail.photo} alt={detail.caption} class="absolute inset-0 w-full h-[420px] object-cover" loading="lazy" />
+                    </a>
+                </div>
+                <form class="flex p-2 max-sm:h-screen">
+                    <div class="flex flex-wrap mt-24">
+                        <h1 class="text-lg font-semibold text-slate-900">
                             {detail.caption}
                         </h1>
                         <div class="w-full flex-none text-sm font-medium text-slate-700 mt-2">
-                            {detail.user}
+                            Created by: <code>{detail.user}</code>
                         </div>
                     </div>
                     <div class="flex space-x-4 mb-6 mt-10 text-sm font-medium">
@@ -42,7 +58,7 @@ function Detail() {
                         </div>
                     </div>
                 </form>
-            </div>
+            </div> */}
         </div>
     )
 }
